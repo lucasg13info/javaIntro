4b7fa2860641f185d5f8f533897ab30cd4bb32ab
Lucas Estefano <lucccasestefano1@gmail.com>
2026-02-08 12:23:44 +1000

    calculatepropertyYield

---
+++ src/com/lucascode/methods/methodRentalYield.java
@@ -0,0 +1,25 @@
+package com.lucascode.methods;
+
+public class methodRentalYield {
+
+    //Method to calculate rental Yields:
+    private static double rentalYield(double rentalIncoming, double purchasedProperty){
+
+        double rentalIncomingYear = (rentalIncoming * 12);
+        double rentalYield = (rentalIncomingYear / purchasedProperty) * 100;
+
+        System.out.println("-----------------------------");
+        System.out.println("Rental incoming a year is: " + rentalIncomingYear);
+        System.out.println("purchasedProperty: " + purchasedProperty);
+        System.out.println("Rental yield is: " + rentalYield);
+        System.out.println("-----------------------------");
+
+        return rentalYield;
+
+    }
+
+    public static void main(String[] args){
+        double yield = rentalYield(1000, 250000);
+
+    }
+}
