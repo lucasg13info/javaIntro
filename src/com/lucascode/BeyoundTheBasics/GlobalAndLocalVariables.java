e8b0723823badd50085a0daa0edaa4ae67600abe
Lucas Estefano <lucccasestefano1@gmail.com>
2026-02-09 21:02:36 +1000

    Global, and local variable

---
+++ src/com/lucascode/BeyoundTheBasics/GlobalAndLocalVariables.java
@@ -0,0 +1,28 @@
+package com.lucascode.BeyoundTheBasics;
+
+public class GlobalAndLocalVariables {
+    //Global variable for the class GlobalAndLocalVariables
+    private static final String BRAND = "Amigoscode";
+
+    //Global variable - accessible from anywhere other classes can see and use.
+    public static final double PI = 3.14;
+
+
+
+    public static void main(String[] args){
+        // Global and local variables
+        //Global can be accesed from this class and other classes
+        //Local: only visible in the scope of the function, only acessible in the scope of the method
+
+        //Local variable:
+        String country = "England";
+
+        System.out.println(BRAND);
+        foo();
+    }
+
+    //Funciton:
+    private static void foo(){
+        System.out.println(BRAND);
+    }
+}
