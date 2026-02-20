68ef9c728b1dab611ac68561653f9d952c5366ea
Lucas Estefano <lucccasestefano1@gmail.com>
2026-02-20 11:02:24 +1000

    Course done

---
+++ src/com/lucascode/OrganizeCode/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.lucascode.OrganizeCode.utils;
+
+public class StringUtils {
+    //Here all classes that the system can use
+
+
+    //Exemple:
+    public  static boolean isEmpty(String s ){
+        return s.isEmpty();
+    }
+}
